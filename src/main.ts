fea4debf6f7b563e999bbd2e85547f745c0e3fe0
ErickjGonzalez <erickjgonzalez117@gmail.com>
2026-06-25 23:59:18 -0600

    railway 2

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,15 @@ import { ValidationPipe, BadRequestException } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-  origin: [
-    'http://localhost:3000',
-    'https://exercise-2-frontend-production.up.railway.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'authorization']
-})
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'authorization']
+  })
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     exceptionFactory: (errors) => new BadRequestException(errors)
   }))
-  await app.listen(3001)
+  await app.listen(process.env.PORT ?? 3001)
 }
 bootstrap()
